29017d0e0b5114e61b53a2ca906489d310fd3285
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2022-10-24 20:59:18 -0500

    commit 3: Test still...

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -5,10 +5,21 @@ public class Main{ //class
 		System.out.println("Hello World");
 		System.out.println("Hi");
 		
+		System.out.println(11*3);
+		System.out.println();
+		
 		int x = 5;
 		int y = 6;
 		int sum = x + y;
 		System.out.println(sum);
+		
+		int myNum = 20;
+		float myFloatNum = 5.909f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hey girl";
+		
+		System.out.println(myText + ", " + "I am " + myLetter + ". I am " + myNum + ". I found a float like " + myFloatNum);
 		
 	}
 }
