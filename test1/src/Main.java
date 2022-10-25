2240147fddb083a0fb0d1f0adbc6e40866390cd2
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2022-10-24 21:15:35 -0500

    commit 4

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -21,5 +21,37 @@ public class Main{ //class
 		
 		System.out.println(myText + ", " + "I am " + myLetter + ". I am " + myNum + ". I found a float like " + myFloatNum);
 		
+		String firstName = "John ";
+		String lastName = "Doe";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
+		
+		int x1, y1, z1;
+		x1 = y1 = z1 = 50;
+		System.out.println(x1 + y1 + z1);
+		
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);
+		System.out.println(isFishTasty);
+		
+		byte myNum1 = 100;
+		System.out.println(myNum1);
+		
+		short myNum2 = 5000;
+		System.out.println(myNum2);
+		
+		int myNum3 = 100000;
+		System.out.println(myNum3);
+		
+		long myNum4 = 15000000000L;
+		System.out.println(myNum4);
+		
+		float myNum5 = 5.75f;
+		System.out.println(myNum5);
+		
+		double myNum6 = 19.99d;
+		System.out.println(myNum6);
+		
 	}
 }
