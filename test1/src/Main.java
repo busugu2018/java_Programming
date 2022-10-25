db131568f98009de0182fa3215ed6078768aa2a3
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2022-10-24 20:36:33 -0500

    Commit 2

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,2 +1,14 @@
-module test1 {
+public class Main{ //class
+	public static void main(String[] args) {//method
+		//write my code
+		System.out.println("Hi");
+		System.out.println("Hello World");
+		System.out.println("Hi");
+		
+		int x = 5;
+		int y = 6;
+		int sum = x + y;
+		System.out.println(sum);
+		
+	}
 }
